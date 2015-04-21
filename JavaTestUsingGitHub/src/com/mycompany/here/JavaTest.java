472d62f8710f50c89731382b8dea6f5903a32541
Napoleon Nazir <napoleon_nazir@smbcnikko.co.jp>
2015-04-21 17:26:52 +0900

    added two lines.

--- JavaTestUsingGitHub/src/com/mycompany/here/JavaTest.java
+++ JavaTestUsingGitHub/src/com/mycompany/here/JavaTest.java
@@ -4,6 +4,8 @@ public class JavaTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
+		System.out.println("heyheyhey");
+		System.out.println("gogogogo");
 		
 	}
 
